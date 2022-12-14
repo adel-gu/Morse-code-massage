bd12a93a08528c15a4957a509e9aa9d587d6f0a4
adelguit <adeelguitoun@gmail.com>
2022-12-14 17:31:49 +0100

    Fix linters errors

--- decrypt.rb
+++ decrypt.rb
@@ -1,51 +1,51 @@
 # Morse code Hash
 MORSE_CODE = {
-  ".-"   => "A",
-  "-..." => "B",
-  "-.-." => "C",
-  "-.."  => "D",
-  "."    => "E",
-  "..-." => "F",
-  "--."  => "G",
-  "...." => "H",
-  ".."   => "I",
-  ".---" => "J",
-  "-.-"  => "K",
-  ".-.." => "L",
-  "--"   => "M",
-  "-."   => "N",
-  "---"  => "O",
-  ".--." => "P",
-  "--.-" => "Q",
-  ".-."  => "R",
-  "..."  => "S",
-  "-"    => "T",
-  "..-"  => "U",
-  "...-" => "V",
-  ".--"  => "W",
-  "-..-" => "X",
-  "-.--" => "Y",
-  "--.." => "Z",
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
-def get_char(c)
+def get_char(code)
   # get the letter from morse code hash
-  return MORSE_CODE[c]
+  MORSE_CODE[code]
 end
 
 # String => String
 # Convert a code char to a latter.
 def decode(code_seq)
   # split code_seq to an array on each tow spaces long
-  code_seq_arr = code_seq.split("  ")
+  code_seq_arr = code_seq.split('  ')
 
   # Hold decoded charachters
-  decoded_seq = ""
+  decoded_seq = ''
 
   # iterate throught each element in the code_seq_arr
   code_seq_arr.each do |code|
     # split each element to an array on each one space long
-    sub_code_seq_arr = code.split(" ")
+    sub_code_seq_arr = code.split
 
     # iterate throught each element in the sub_code_seq_arr and decode each element.
     sub_code_seq_arr.each do |c|
@@ -53,11 +53,11 @@ def decode(code_seq)
     end
 
     # when the loop ends this means creating a space between words.
-    decoded_seq += " "
+    decoded_seq += ' '
   end
 
   # return the result
-  return decoded_seq
+  decoded_seq
 end
 
-puts(decode("-- -.--   -. .- -- ."))
+puts(decode('-- -.--   -. .- -- .'))
